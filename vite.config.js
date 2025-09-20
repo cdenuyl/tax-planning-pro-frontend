d97c148b8d6fd9f51f630b66dd47a23355fe1700
User <user@example.com>
2025-09-20 16:57:35 -0400

    Fix: Add jspdf to rollupOptions.external in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["jspdf"],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
